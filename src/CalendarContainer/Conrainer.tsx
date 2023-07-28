6386b0b4649eda82e3ace5954a02e70fc3d79399
Кабанов Дмитрий Алексеевич <d.a.kabanov@samsmu.ru>
2023-07-28 22:40:34 +0300

    refactoring

--- src/CalendarContainer/Conrainer.tsx
+++ src/CalendarContainer/Conrainer.tsx
@@ -1,34 +1,19 @@
 import React from "react";
 import "./Conrainer.css";
-import {
-    MONTHS_MAPPER,
-    WEEK_DAYS_RUS_MAPPER,
-    onGetDaysInMonth,
-    onGetOtherMonthDays,
-} from "./utils";
-import { Day } from "./Day/Day";
+import { MONTHS_MAPPER, onGetDaysInMonth, onGetOtherMonthDays } from "./utils";
+import { DaysList } from "./DaysList/DaysList";
 
 export const Conrainer = () => {
-    const currentDate = new Date();
-    const initialMonth = currentDate.getMonth() + 1;
-    const initialYear = currentDate.getFullYear();
-    const [currnetMonth, setCurrnetMonth] = React.useState(initialMonth);
-    const [currentYear, setCurrentYear] = React.useState(initialYear);
-    const currentDaysList = onGetDaysInMonth(currnetMonth, currentYear);
-    const initWeekDayInMonth = currentDaysList[0].getDay();
-    const endWeekDayInMonth =
-        currentDaysList[currentDaysList.length - 1].getDay();
-    const weekDays = new Array(7).fill(null);
-    const prevDays = onGetOtherMonthDays(
-        onGetDaysInMonth(currnetMonth, currentYear),
-        initWeekDayInMonth,
-        "prev"
-    );
-    const nexDays = onGetOtherMonthDays(
-        onGetDaysInMonth(currnetMonth, currentYear),
-        endWeekDayInMonth,
-        "next"
-    );
+    const currentDate                       =   new Date();
+    const initialMonth                      =   currentDate.getMonth() + 1;
+    const initialYear                       =   currentDate.getFullYear();
+    const [currnetMonth, setCurrnetMonth]   =   React.useState(initialMonth);
+    const [currentYear, setCurrentYear]     =   React.useState(initialYear);
+    const currentDaysList                   =   onGetDaysInMonth(currnetMonth, currentYear);
+    const initWeekDayInMonth                =   currentDaysList[0].getDay();
+    const endWeekDayInMonth                 =   currentDaysList[currentDaysList.length - 1].getDay();
+    const prevDays                          =   onGetOtherMonthDays(onGetDaysInMonth(currnetMonth, currentYear), initWeekDayInMonth, "prev");
+    const nexDays                           =   onGetOtherMonthDays(onGetDaysInMonth(currnetMonth, currentYear), endWeekDayInMonth, "next");
 
     const onSwitchMonth = (option: "next" | "prev") => {
         if (option === "next") {
@@ -48,32 +33,15 @@ export const Conrainer = () => {
 
     return (
         <div className="custom_calendar__container">
-            <div className="custom_years_control__container">
-                <div onClick={() => onSwitchYear("prev")}>
-                    {currentYear - 1}
-                </div>
-                <div>{currentYear}</div>
-                <div onClick={() => onSwitchYear("next")}>
-                    {currentYear + 1}
-                </div>
-            </div>
-            <div className="custom_month_control__container">
-                <div onClick={() => onSwitchMonth("prev")}>&#60;</div>
-                <span>{MONTHS_MAPPER[currnetMonth]}</span>
-                <div onClick={() => onSwitchMonth("next")}>&#62;</div>
-            </div>
-            <>
-                <div className="custom_calendar_content__wrapper">
-                    {weekDays.map((_, idx) => (
-                        <div key={idx} className="custom_day_title__container">
-                            {WEEK_DAYS_RUS_MAPPER[idx]}
-                        </div>
-                    ))}
-                    <Day days={prevDays} kind="prev" />
-                    <Day days={currentDaysList} kind="current" />
-                    <Day days={nexDays} kind="next" />
-                </div>
-            </>
+            <DaysList
+                currentDaysList={currentDaysList}
+                nexDays={nexDays}
+                prevDays={prevDays}
+                currnetMonth={MONTHS_MAPPER[currnetMonth]}
+                currentYear={currentYear}
+                onSwitchMonth={onSwitchMonth}
+                onSwitchYear={onSwitchYear}
+            />
         </div>
     );
 };
--- src/CalendarContainer/DaysList/DaysList.tsx
+++ src/CalendarContainer/DaysList/DaysList.tsx
@@ -1,3 +1,53 @@
-export const DaysList = () => {
-    return <></>
-}
+import { Day } from "../Day/Day";
+import { WEEK_DAYS_RUS_MAPPER } from "../utils";
+import "./DaysList.css";
+
+export const DaysList: React.FC<{
+    currnetMonth: string;
+    currentYear: number;
+    prevDays: Date[];
+    currentDaysList: Date[];
+    nexDays: Date[];
+    onSwitchYear: (option: "next" | "prev") => void;
+    onSwitchMonth: (option: "next" | "prev") => void;
+}> = (props) => {
+    const {
+        currnetMonth,
+        currentYear,
+        prevDays,
+        currentDaysList,
+        nexDays,
+        onSwitchYear,
+        onSwitchMonth,
+    } = props;
+
+    const weekDays = new Array(7).fill(null);
+    return (
+        <>
+            <div className="custom_years_control__container">
+                <div onClick={() => onSwitchYear("prev")}>
+                    {currentYear - 1}
+                </div>
+                <div>{currentYear}</div>
+                <div onClick={() => onSwitchYear("next")}>
+                    {currentYear + 1}
+                </div>
+            </div>
+            <div className="custom_month_control__container">
+                <div onClick={() => onSwitchMonth("prev")}>&#60;</div>
+                <span>{currnetMonth}</span>
+                <div onClick={() => onSwitchMonth("next")}>&#62;</div>
+            </div>
+            <div className="custom_dayslist_content__wrapper">
+                {weekDays.map((_, idx) => (
+                    <div key={idx} className="custom_day_title__dayslist">
+                        {WEEK_DAYS_RUS_MAPPER[idx]}
+                    </div>
+                ))}
+                <Day days={prevDays} kind="prev" />
+                <Day days={currentDaysList} kind="current" />
+                <Day days={nexDays} kind="next" />
+            </div>
+        </>
+    );
+};
