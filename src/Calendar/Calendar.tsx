1562732dcefe2f69f4dc36729d09055ef7bdf7a8
Кабанов Дмитрий Алексеевич <d.a.kabanov@samsmu.ru>
2023-07-28 20:33:14 +0300

    format code

--- src/Calendar/Calendar.tsx
+++ src/Calendar/Calendar.tsx
@@ -5,39 +5,38 @@ import {
     WEEK_DAYS_MAPPER,
     WEEK_DAYS_RUS_MAPPER,
     onGetDaysInMonth,
-    onGetIsoStringWithNoTimeZone,
     onGetPrevDays,
 } from "./utils";
 import { Days } from "./Day/Days";
 
 export const Calendar = () => {
-    const currentDate = new Date("2023/07/25");
-    const _currnetMonth = currentDate.getMonth() + 1;
-    const _currentYear = currentDate.getFullYear();
 
-    const [currnetMonth, setCurrnetMonth] = React.useState(_currnetMonth);
-    const [currentYear, setCurrentYear] = React.useState(_currentYear);
-    const currentDaysList = onGetDaysInMonth(currnetMonth, currentYear);
-    const _firstWeekDay = currentDaysList[0].getDay();
-    const _lastWeekDay = currentDaysList[currentDaysList.length - 1].getDay();
-    const weekDays = new Array(7).fill(null);
-    const firstWeekDay = WEEK_DAYS_MAPPER[_firstWeekDay];
-    const lastWeekDay = WEEK_DAYS_MAPPER[_lastWeekDay];
-    const prevDays = onGetPrevDays(
-        onGetDaysInMonth(
-            currnetMonth > 1 ? currnetMonth - 1 : 12,
-            currnetMonth > 1 ? currentYear : currentYear - 1
-        ),
-        firstWeekDay
-    );
 
-    const nexDays = onGetDaysInMonth(currnetMonth + 1, currentYear).slice(
-        0,
-        6 - lastWeekDay
-    );
-    const onClickDay = (day: Date) => {
-        alert(onGetIsoStringWithNoTimeZone(day));
-    };
+    const currentDate =                         new Date("2023/07/25");
+    const initialMonth =                        currentDate.getMonth() + 1;
+    const initialYear =                         currentDate.getFullYear();
+    const [currnetMonth, setCurrnetMonth] =     React.useState(initialMonth);
+    const [currentYear, setCurrentYear] =       React.useState(initialYear);
+    const currentDaysList =                     onGetDaysInMonth(currnetMonth, currentYear);
+    const initWeekDayInMonth =                  currentDaysList[0].getDay();
+    const endWeekDayInMonth =                   currentDaysList[currentDaysList.length - 1].getDay();
+    const weekDays =                            new Array(7).fill(null);
+    const firstWeekDay =                        WEEK_DAYS_MAPPER[initWeekDayInMonth];
+    const lastWeekDay =                         WEEK_DAYS_MAPPER[endWeekDayInMonth];
+    const prevDays =                            onGetPrevDays(
+                                                    onGetDaysInMonth(
+                                                        currnetMonth > 1 ? currnetMonth - 1 : 12,
+                                                        currnetMonth > 1 ? currentYear : currentYear - 1
+                                                    ),
+                                                    firstWeekDay
+                                                );
+
+    const nexDays =                             onGetDaysInMonth(currnetMonth + 1, currentYear)
+                                                    .slice(0, 6 - lastWeekDay);
+
+
+
+
 
     // console.log("==> " , foo(12, 12, "+"))
 
